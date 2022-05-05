3169014067eeb9fb8bf19fb87d9f382360986f35
Volodymyr Sylantiev/WEB Security Solutions /SRUKR/Designer/Samsung Electronics <v.sylantiev@samsung.com>
2022-05-05 19:10:29 +0300

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+const weatherMsg = document.querySelector('#message');
+const weatherForm = document.querySelector('form');
+const searchInput = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const location = searchInput.value;
+
+  weatherMsg.textContent = 'Loading...';
+
+  fetch('http://localhost:3000/weather?address=' + location, {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.error) {
+        weatherMsg.textContent = data.error;
+      } else {
+        weatherMsg.textContent = data.location + '. ' + data.forcast;
+      }
+    });
+});
---
+++ src/app.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+
+const geocode = require('../../weather-app/utils/geocode');
+const forecast = require('../../weather-app/utils/weather');
+
+const app = express();
+
+//define static directory path
+app.use(express.static(path.join(__dirname, '../public')));
+//define path
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+//set up
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather app',
+    name: 'svetouch',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About app',
+    name: 'svetouch',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    message: 'Help message.',
+    name: 'svetouch',
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.send('Help article not found.');
+});
+
+//get weather by ?address=
+app.get('/weather', (req, res) => {
+  geocode(req.query.address, (error, { latitude, longtitude, geolocation } = {}) => {
+    if (error) {
+      return res.send(error ? error : 'No address provided');
+    }
+    forecast([latitude, longtitude], (error, { temperature, location, description }) => {
+      if (error) {
+        res.send(error);
+      }
+
+      res.send({
+        forcast: 'It is currently ' + temperature + ' degrees out in ' + location + '. ' + description + '.',
+        location: geolocation,
+        address: req.query.address,
+      });
+    });
+  });
+});
+
+app.get('/products', (req, res) => {
+  res.send({
+    products: [],
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('error', {
+    title: 'Error',
+    message: 'page not found',
+  });
+});
+
+//define localhost port
+app.listen(3000, () => console.log('Server is up on port 3000'));
